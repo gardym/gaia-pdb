771ba40b90cd3578a635c8bc3f321e52fbff3eb8
Nathan Jones <thenathanjones@gmail.com>
2011-10-11 20:27:55 +0800

    Blank Rails 3.1 scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gaia::Application.config.secret_token = '1cc3640ef5ad003e54a08e616d38fad1c5676d743e2560baedfc2d9726e04322dac788e0ed8e1495483eb5f4fb46985442fc347060424452a90e09d26e2c8cf9'

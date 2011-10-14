72a275848d7d7c3337f458e88071759635f5b0b7
Kari Barnard <kmoran@KMac.local>
2011-10-14 16:07:42 +0800

    Added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Gaia::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  root :to => "home#index"
   resources :parameters do
     collection do
       get 'search'

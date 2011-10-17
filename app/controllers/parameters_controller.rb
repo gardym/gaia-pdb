59fc16d9bedae9173ab9e1e9de1acd1e9f5a403b
silvio <montanari.silvio@gmail.com>
2011-10-17 09:06:17 +0800

    added 'LIKE' query functionality for source, unit, description

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,5 +1,52 @@
 class ParametersController < ApplicationController
-  def index
-    @parameters = Parameter.all
+  class SearchFilter
+    @@criteria_field_map = {
+      "unit" => "unit", 
+      "source" => "source", 
+      "description" => "description"
+    }
+    class_eval { @@criteria_field_map.each_key {|c| attr_reader c} }
+    
+    def self.initialize_from params
+      valid_criteria = params.select do |k,v|
+        @@criteria_field_map.has_key?k and !v.nil? and !v.empty?
+      end
+      sc = new
+      sc.instance_variable_set(:@criteria, Hash[valid_criteria.map {|k,v| [@@criteria_field_map[k], v]}])
+      return sc
+    end
+    
+    def empty?
+      @criteria.nil? or @criteria.empty?
+    end
+    
+    def build_query
+      where_condition = @criteria.keys.first + @criteria.keys.drop(1).inject(" LIKE ?") {|q, p| q + " AND #{p} LIKE ?"}
+      return where_condition, @criteria.values.map{|v| "%#{v}%"}
+    end
+    
   end
+  
+  def index
+    retrieve_parameters
+  end
+  
+  def search
+    unless request.query_string.empty?
+      retrieve_parameters SearchFilter.initialize_from(params)
+      render :index 
+    end    
+  end
+
+  private
+  
+  def retrieve_parameters(filter = SearchFilter.new)
+    if filter.empty?
+      @parameters = Parameter.all
+    else
+      query, params = filter.build_query
+      @parameters = Parameter.where(query, *params)
+    end    
+  end
+  
 end
--- spec/controllers/parameters_controller_spec.rb
+++ spec/controllers/parameters_controller_spec.rb
@@ -3,10 +3,43 @@ require 'spec_helper'
 describe ParametersController do
   describe "GET index"
     it "should return all parameters" do
-      Parameter.stub!(:all) {:some_params}
+      Parameter.stub(:all) {:some_params}
       
       get :index
       
       assigns(:parameters).should == :some_params
     end
+    
+    describe "GET search"
+      it "should render the search form page when no query string" do
+        get :search
+        response.should render_template("search")
+      end
+      
+      it "should return all parameters when search criteria are empty" do
+        Parameter.stub(:all) {:some_params}
+        
+        get :search, {:source => "", :unit => "", :description => ""}
+        
+        assigns(:parameters).should == :some_params
+        response.should render_template("index")
+      end
+      
+      it "should return parameters satisfying one search criteria" do
+        Parameter.stub(:where).with("description LIKE ?", "%test.description%").and_return(:some_params)
+        
+        get :search, {:description => "test.description"}
+        
+        assigns(:parameters).should == :some_params
+        response.should render_template("index")        
+      end
+
+      it "should return parameters satisfying multiple search criteria" do
+        Parameter.stub(:where).with("unit LIKE ? AND description LIKE ?", "%test.unit%", "%test.description%").and_return(:some_params)
+        
+        get :search, {:unit => "test.unit", :description => "test.description"}
+        
+        assigns(:parameters).should == :some_params
+        response.should render_template("index")        
+      end
 end

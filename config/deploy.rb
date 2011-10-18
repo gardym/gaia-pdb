888132ca36ef93f51c2b35345f21784ea63cfa3a
Nathan Jones <thenathanjones@gmail.com>
2011-10-18 14:47:46 +0800

    Moved the PID file out of the current directory as we can't reference it because the symlink is created first

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,14 +21,14 @@ set :use_sudo, false
 ssh_options[:forward_agent] = true
 
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :unicorn_pid, "#{deploy_to}/unicorn.pid"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do 
     run "cd #{current_path} && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do 
-    run "if [ -f #{unicorn_pid} ] ; then cd #{current_path} && #{try_sudo} kill `cat #{unicorn_pid}` ; fi"
+    run "if [ -f #{unicorn_pid} ] ; then echo 'ohai' && cd #{current_path} && #{try_sudo} kill `cat #{unicorn_pid}` ; fi"
   end
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
     run "if [ -f #{unicorn_pid} ] ; then cd #{current_path} && #{try_sudo} kill -s QUIT `cat #{unicorn_pid}` ; fi"
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,7 +18,7 @@ preload_app false
 worker_processes 4
 
 # Where to drop a pidfile
-pid '/var/www/gaia/current/tmp/pids/unicorn.pid'
+pid '/var/www/gaia/unicorn.pid'
 
 # Where stderr gets logged
 stderr_path '/var/log/unicorn/error.log'

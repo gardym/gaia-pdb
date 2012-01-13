f3836cbd2e7f061bc7a3a7e96086a3495b8d300d
Tobias Lauchenauer <lauchenauer@ubuntu.(none)>
2012-01-12 19:32:54 -0800

    added initial export to xml and pdf to controller

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'will_paginate', '~> 3.0.2'
 gem 'haml'
 gem 'jquery-rails'
 gem "therubyracer", :require => 'v8'
+gem "prawn"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,3 +1,5 @@
+require "prawn"
+
 class ParametersController < ApplicationController
   
   def index
@@ -7,8 +9,27 @@ class ParametersController < ApplicationController
   def search
     unless request.query_string.empty?
       @parameters = Parameter.search params[:page], SearchFilter.initialize_from(params)
-      render :index 
-    end    
+
+      respond_to do |format|
+        format.html { render :index }
+        format.xml { render :xml => @parameters }
+        format.pdf { render :text => create_pdf(@parameters) }
+      end
+    end
   end
-    
+
+  def create_pdf(parameters)
+    pdf = Prawn::Document.new
+
+    data = [["Unit", "Source", "Expression", "Description"]]
+
+    parameters.first(4).each do |p|
+      data += [[p.unit, p.source, p.expression, p.description]]
+    end
+
+    pdf.table(data, :header => true)
+
+    pdf.render
+  end
+
 end
